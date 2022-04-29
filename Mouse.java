96ba99be4799180aef3e5090f253a796534663c0
Alexlee8888 <65263737+Alexlee8888@users.noreply.github.com>
2022-04-29 08:28:19 -0700

    created location class

--- Mouse.java
+++ Mouse.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
 public class Mouse {
-    
+
+    private ArrayList<Location> position;
+    public Mouse() {
+        // initalize list and add some static numbers
+    }
+
+
 }
