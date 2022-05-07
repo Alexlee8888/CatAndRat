7631da874d54579aa22c3e05262f46d225449f20
Alexlee8888 <65263737+Alexlee8888@users.noreply.github.com>
2022-05-07 13:49:37 -0700

    idk

---
+++ THINGYCHANGE.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import org.w3c.dom.events.MouseEvent;
+import javax.swing.*;
+import java.awt.*;
+
+public class THINGYCHANGE {
+    private InputHandler inputHandler;
+    private static Image image;
+    public THINGYCHANGE(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
+    }
+}
