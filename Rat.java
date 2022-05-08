1079538d084d695576b3c8da1082f0ff93de5304
Alexlee8888 <65263737+Alexlee8888@users.noreply.github.com>
2022-05-07 20:16:00 -0700

    got image for rat

--- Rat.java
+++ Rat.java
@@ -7,8 +7,10 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
 
-public class Rat extends Rectangle {
+public class Rat extends THINGYCHANGE {
 
     // private Location position;
     // private double orientation; // degree
@@ -33,18 +35,33 @@ public class Rat extends Rectangle {
 
     private int dx;
     private int dy;
+    private int x;
+    private int y;
+    private double angle;
     private InputHandler inputHandler;
+    private static int width = 50;
+    private static int height = 50;
+    private static Image ratImage = Toolkit.getDefaultToolkit().getImage("rat.png");
 
-    public Rat(int x, int y, int width, int height, int dx, int dy, InputHandler inputHandler) {
-        setBounds(x, y, width, height);
+    // public Rat(int x, int y, int width, int height, int dx, int dy, InputHandler inputHandler) {
+    //     setBounds(x, y, width, height);
+    //     this.dx = dx;
+    //     this.dy = dy;
+    //     this.inputHandler = inputHandler;
+    // }
+    
+    public Rat(int x, int y, int dx, int dy, double angle, InputHandler inputHandler) {
+        this.x = x;
+        this.y = y;
         this.dx = dx;
         this.dy = dy;
+        this.angle = angle;
         this.inputHandler = inputHandler;
     }
 
     public void update() {
-        this.x += dx;
-        this.y += dy;
+        x += dx;
+        y += dy;
         if(inputHandler.isKeyPressed(KeyEvent.VK_D) || inputHandler.isKeyPressed(KeyEvent.VK_RIGHT)) {
             setDx(3);
         }
@@ -69,7 +86,14 @@ public class Rat extends Rectangle {
 
     public void draw(Graphics g) {
         g.setColor(Color.black);
-        g.fillRect(this.x, this.y, this.width, this.height);
+        Graphics2D g2d = (Graphics2D) g;
+        // draw rect
+
+        AffineTransform identity = new AffineTransform();
+
+        g2d.setTransform(identity);
+        g2d.rotate(Math.toRadians(angle), x, y);
+        g.drawImage(ratImage, x - (width/2), y - (height/2), width, height, null);
     }
 
     public void setDx(int dx) {
--- THINGYCHANGE.java
+++ THINGYCHANGE.java
@@ -3,10 +3,7 @@ import org.w3c.dom.events.MouseEvent;
 import javax.swing.*;
 import java.awt.*;
 
+
 public abstract class THINGYCHANGE {
     private InputHandler inputHandler;
-    private static Image image;
-    public THINGYCHANGE(InputHandler inputHandler) {
-        this.inputHandler = inputHandler;
-    }
 }
