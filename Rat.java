05b2cbfc129b25d8e3fce80343dc5de0ae4cf218
Alex Lee <alexleeyt8888@gmail.com>
2022-05-10 12:54:42 -0700

    changed rat movement stuff

--- Rat.java
+++ Rat.java
@@ -1,76 +1,63 @@
-import java.util.ArrayList;
-import org.w3c.dom.events.MouseEvent;
-import javax.swing.*;
-import java.awt.event.KeyEvent;
-import java.awt.Rectangle;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.geom.AffineTransform;
 
 public class Rat extends THINGYCHANGE {
 
-    // private int dx;
-    // private int dy;
     private double x;
     private double y;
     private double angle;
+
+    private double v;
+
     private InputHandler inputHandler;
-    private static int width = 100;
-    private static int height = 100;
+    private static int division = 9;
+    private static int width = 815 / division;
+    private static int height = 1062 / division;
     private static Image ratImage = Toolkit.getDefaultToolkit().getImage("rat.png");
     private static int DEGREES_TURNED = 4;
-    private static int RAT_SPEED = 5;
+    private static int RAT_SPEED = 9;
     private static int MOVE_FORWARD = 1;
     private static int MOVE_BACKWARD = -1;
-    // public Rat(int x, int y, int width, int height, int dx, int dy, InputHandler inputHandler) {
-    //     setBounds(x, y, width, height);
-    //     this.dx = dx;
-    //     this.dy = dy;
-    //     this.inputHandler = inputHandler;
-    // }
-    
+
+
     public Rat(double x, double y, double angle, InputHandler inputHandler) {
         this.x = x;
         this.y = y;
-        // this.dx = dx;
-        // this.dy = dy;
+
         this.angle = angle;
         this.inputHandler = inputHandler;
     }
 
     public void update() {
-        // x += dx;
-        // y += dy;
-        if(inputHandler.isKeyPressed(KeyEvent.VK_D) || inputHandler.isKeyPressed(KeyEvent.VK_RIGHT)) {
+        if (inputHandler.isKeyPressed(KeyEvent.VK_D) || inputHandler.isKeyPressed(KeyEvent.VK_RIGHT)) {
             angle += DEGREES_TURNED;
-        }
-        else if(inputHandler.isKeyPressed(KeyEvent.VK_A) || inputHandler.isKeyPressed(KeyEvent.VK_LEFT)) {
+        } else if (inputHandler.isKeyPressed(KeyEvent.VK_A) || inputHandler.isKeyPressed(KeyEvent.VK_LEFT)) {
             angle -= DEGREES_TURNED;
         }
-        if(inputHandler.isKeyPressed(KeyEvent.VK_W) || inputHandler.isKeyPressed(KeyEvent.VK_UP)) {
+        if (inputHandler.isKeyPressed(KeyEvent.VK_W) || inputHandler.isKeyPressed(KeyEvent.VK_UP)) {
             moveRat(MOVE_FORWARD);
-        }
-        else if(inputHandler.isKeyPressed(KeyEvent.VK_S) || inputHandler.isKeyPressed(KeyEvent.VK_DOWN)) {
+        } else if (inputHandler.isKeyPressed(KeyEvent.VK_S) || inputHandler.isKeyPressed(KeyEvent.VK_DOWN)) {
             moveRat(MOVE_BACKWARD);
         }
 
-        // if(inputHandler.isKeyUp(KeyEvent.VK_D) || inputHandler.isKeyUp(KeyEvent.VK_RIGHT)||inputHandler.isKeyUp(KeyEvent.VK_A) || inputHandler.isKeyUp(KeyEvent.VK_LEFT)) {
-        //     setDx(0);
-        // }
-        // if(inputHandler.isKeyUp(KeyEvent.VK_W) || inputHandler.isKeyUp(KeyEvent.VK_UP) || inputHandler.isKeyUp(KeyEvent.VK_S) || inputHandler.isKeyUp(KeyEvent.VK_DOWN)) {
-        //     setDy(0);
-        // }
-        
+        double deltaX = v * Math.cos(Math.toRadians(angle));
+        double deltaY = v * Math.sin(Math.toRadians(angle));
+        x += deltaX;
+        y += deltaY;
+
+        v *= 0.95;
+
     }
 
     public void moveRat(int forwardBackward) {
-        double deltaX = forwardBackward * (RAT_SPEED*Math.cos(Math.toRadians(angle)));
-        double deltaY = forwardBackward * (RAT_SPEED*Math.sin(Math.toRadians(angle)));
-        x += deltaX;
-        y += deltaY;
+        v += forwardBackward;
+        v = Math.min(v, RAT_SPEED);
+        v = Math.max(v, -RAT_SPEED);
     }
 
     public void draw(Graphics g) {
@@ -82,16 +69,8 @@ public class Rat extends THINGYCHANGE {
 
         g2d.setTransform(identity);
         g2d.rotate(Math.toRadians(angle), x, y);
-        g.drawImage(ratImage, (int) (x - (width/2)), (int) (y - (height/2)), width, height, null);
+        g.drawImage(ratImage, (int) (x - (width / 2)), (int) (y - (height / 2)), width, height, null);
     }
-
-    // public void setDx(int dx) {
-    //     this.dx = dx;
-    // }
-
-    // public void setDy(int dy) {
-    //     this.dy = dy;
-    // }
 
     public void setAngle(int angle) {
         this.angle = angle;
