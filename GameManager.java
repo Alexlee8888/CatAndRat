0c2d1edab332e150b8348a6a8374511d35f7af86
Alex Lee <alexleeyt8888@gmail.com>
2022-05-17 11:15:22 -0700

    blah

--- GameManager.java
+++ GameManager.java
@@ -3,17 +3,21 @@ import javax.swing.plaf.ButtonUI;
 import javax.swing.plaf.DimensionUIResource;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
+import java.util.TreeMap;
 
 public class GameManager {
     private InputHandler inputHandler;
     private boolean isMultiplayer;
     private Player player1;
     private Player player2;
+    private TreeMap<Integer, ArrayList<String>> leaderboard;
     // only for testing
     // private Rat rat;
     // private Cat cat;
 
     GameManager(InputHandler inputHandler, boolean isMultiplayer) {
+        leaderboard = new TreeMap<>();
         this.inputHandler = inputHandler;
         this.isMultiplayer = isMultiplayer;
         player1 = new Person(true, inputHandler); //player 1 is cat, player 2 is rat
@@ -87,11 +91,11 @@ public class GameManager {
 
     public void render(Graphics g) {
         if(player1.isCat()) {
-            player1.getCat().draw(g);
+            // player1.getCat().draw(g);
             player2.getRat().draw(g);
         } else {
             player1.getRat().draw(g);
-            player2.getCat().draw(g);
+            // player2.getCat().draw(g);
         }
 
         // render cat
